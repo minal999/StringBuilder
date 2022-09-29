3f6b13afaa4669ec86af61a019b0d7dfdd4fc049
minal999 <minughadge555@gmail.com>
2022-09-29 21:01:51 +0530

    commit

--- StringBuilder/Program.cs
+++ StringBuilder/Program.cs
@@ -10,25 +10,42 @@ namespace Stringbuilder
     {
         static void Main(string[] args)
         {
-            StringBuilder stringbuilder = new StringBuilder("Hello to all");
-            float salary = 25000.00f;
-            Console.WriteLine(stringbuilder);
+            //StringBuilder stringbuilder = new StringBuilder("Hello to all");
+            //float salary = 25000.00f;
+            //Console.WriteLine(stringbuilder);
 
-            stringbuilder.Append(" My name is Rohan");
-            Console.WriteLine(stringbuilder);
+            //stringbuilder.Append(" My name is Rohan");
+            //Console.WriteLine(stringbuilder);
 
-            stringbuilder.AppendFormat(" my salary is {0:C}", salary);
-            Console.WriteLine(stringbuilder);
+            //stringbuilder.AppendFormat(" my salary is {0:C}", salary);
+            //Console.WriteLine(stringbuilder);
 
-            stringbuilder.Insert(10, " HI ");
-            Console.WriteLine(stringbuilder);
+            //stringbuilder.Insert(10, " HI ");
+            //Console.WriteLine(stringbuilder);
 
-            stringbuilder.Replace("Rohan", "Suraj");
-            Console.WriteLine(stringbuilder);
+            //stringbuilder.Replace("Rohan", "Suraj");
+            //Console.WriteLine(stringbuilder);
 
-            stringbuilder.Remove(6, 11);
-            Console.WriteLine(stringbuilder);
+            //stringbuilder.Remove(6, 11);
+            //Console.WriteLine(stringbuilder);
 
+
+
+            StringBuilder stringBuilder = new StringBuilder("Hello to all");
+            Console.WriteLine("------string builder class----------");
+            Console.WriteLine(stringBuilder);
+            Console.WriteLine(stringBuilder.GetHashCode());
+            stringBuilder.Append(" My name is Rohan");
+            Console.WriteLine(stringBuilder);
+            Console.WriteLine(stringBuilder.GetHashCode());
+            Console.WriteLine("------string class ---------");
+            string s1 = "Hello to all";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.GetHashCode());
+
+            s1 = s1 + " This is sample code";
+            Console.WriteLine(s1);
+            Console.WriteLine(s1.GetHashCode());
 
         }
     }

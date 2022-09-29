313d7acadc8fbaf8f039b3394723ec909a5182c8
minal999 <minughadge555@gmail.com>
2022-09-29 20:58:53 +0530

    Add project files.

---
+++ StringBuilder/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stringbuilder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder stringbuilder = new StringBuilder("Hello to all");
+            float salary = 25000.00f;
+            Console.WriteLine(stringbuilder);
+
+            stringbuilder.Append(" My name is Rohan");
+            Console.WriteLine(stringbuilder);
+
+            stringbuilder.AppendFormat(" my salary is {0:C}", salary);
+            Console.WriteLine(stringbuilder);
+
+            stringbuilder.Insert(10, " HI ");
+            Console.WriteLine(stringbuilder);
+
+            stringbuilder.Replace("Rohan", "Suraj");
+            Console.WriteLine(stringbuilder);
+
+            stringbuilder.Remove(6, 11);
+            Console.WriteLine(stringbuilder);
+
+
+        }
+    }
+}
